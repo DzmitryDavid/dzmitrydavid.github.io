7b198f8b9486470e54eb974edb70d22504f68782
Dzmitry <Dzmitrydavidovich89@gmail.com>
2021-07-21 03:22:06 +0300

    add about component and styling for it

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import AboutSection from "./components/AboutSection";
 
 function App() {
   return (
     <div className="App">
-      
+      <AboutSection/>
     </div>
   );
 }
---
+++ src/components/AboutSection.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { About, Hide, Description, Image } from '../styles';
+
+
+const AboutSection = () => {
+  
+  return (
+    <About>
+      <Description>
+        <div >
+          <Hide>
+            <h2 >
+            Dzmitry Davidovich
+            </h2>
+          </Hide>
+          <Hide>
+            <h2>Front <span> End</span> 
+            </h2>
+          </Hide>
+          <Hide>
+            <h2>developer.</h2>
+          </Hide>
+        </div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi a iste asperiores vel doloribus   recusandae. Nihil, rem placeat?</p>
+          <button>Contact me</button>
+          </Description>  
+          <Image>
+            <img src="" alt="Dzmitry" />
+          </Image>
+    </About>
+  )
+}
+
+
+
+export default AboutSection;
