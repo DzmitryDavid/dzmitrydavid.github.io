5e528ac04eab6e1195265b129a0ec0b653d4febf
Dzmitry <Dzmitrydavidovich89@gmail.com>
2021-07-21 13:51:56 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import { Route, Switch } from 'react-router';
+
 import AboutSection from "./components/AboutSection";
 import Nav from './components/Nav';
-
+import Contacts from './pages/Contacts';
 import GlobalStyles from './GlobalStyles';
+import ProjectDetail from './pages/ProjectDetail'
 
 function App() {
   return (
     <div className="App">
-      <GlobalStyles />
-      <Nav/>
-      <AboutSection/>
+      <GlobalStyles/>
+        <Nav/>
+      <Switch>
+        <Route exact path="/">
+          <AboutSection />
+        </Route>
+        <Route path="/contacts">
+          <Contacts />
+        </Route>
+        {/* <Route path="/projects/:id">
+          <ProjectDetail/>
+        </Route> */}
+        
+      </Switch>
     </div>
   );
 }
